e29fe58d7a3655550a39ed387968027be7684ab9
pablo <pablo@DESKTOP-0TFARTD>
2019-01-14 10:15:19 -0400

    Esta es una Prueba

---
+++ PruebaMVC/PruebaMVC/Models/PruebaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaMVC.Models
+{
+    public class PruebaModel
+    {
+        public int intPrueba { get; set; }
+        public string stringPrueba { get; set; }
+    }
+}
