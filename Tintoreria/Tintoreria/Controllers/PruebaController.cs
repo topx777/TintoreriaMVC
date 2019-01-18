e1c451ad7813ff36f4066ed87cc209ca32239e23
Pablo Fernando H. Y. <pablohy2898@hotmail.com>
2019-01-18 09:45:41 -0400

    prueba de git

---
+++ Tintoreria/Tintoreria/Controllers/PruebaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Upds.Sistemas.ProgWeb2.Tintoreria.MVC.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
