55f9fbd3eb9f773d1aba2efb26f30597ef18e122
Pablo Fernando H. Y. <pablohy2898@hotmail.com>
2019-01-20 16:05:07 -0400

    cracion Usuario Dal

---
+++ Tintoreria/TintoreriaDAL/UsuarioDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Upds.Sistemas.ProgWeb2.Tintoreria.Core;
+
+
+namespace Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaDAL
+{
+    class UsuarioDal
+    {
+    }
+}
