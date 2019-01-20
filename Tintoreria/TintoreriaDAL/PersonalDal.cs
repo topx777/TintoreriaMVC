e41450dd5578945dfdbc48995a41f6cb3d28ccbb
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-01-20 16:29:45 -0400

    cambios clase telefeono aniadida

--- Tintoreria/TintoreriaDAL/PersonalDal.cs
+++ Tintoreria/TintoreriaDAL/PersonalDal.cs
@@ -18,27 +18,40 @@ namespace Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaDAL
             SqlCommand command = null;
 
             //Consulta para insertar datos de personal
-            string queryString = @"INSERT INTO  (Nombre,PrimerApellido,SegundoApellido,Sexo,FechaNacimiento,Correo,
-                                 CodPersonal,Ci,FechaIngreso,Cargo,Sueldo,Turno)
-                                 VALUES
-                                 (@nombre, @primerApellido, @segundoApellido, @sexo, @fechaNacimiento, @correo,
-                                 @codPersonal, @ci, fechaIngreso, @cargo, @sueldo, @turno)";
+            string queryString = @"INSERT INTO Usuario(Username, Password, EsAdmin) VALUES(@username, @password, @esAdmin)";
+            string queryString1 = @"INSERT INTO Persona(Nombre, PrimerApellido, SegundoApellido, Sexo, FechaNacimiento, Correo, Usuario)
+                                  VALUES(@nombre, @primerApellido, @segundoApellido, @sexo, @fechaNacimiento, @correo, @usuario)";
+            string queryString2 = @"INSERT INTO Cliente(IdPersona, Nit, Razon, FechaRegistro) VALUES (@idPersona ,@nit, @razon, @fechaRagistro)";
             try
             {
                 command = Methods.CreateBasicCommand(queryString);
+                command.Parameters.AddWithValue("@username", personal.Usuario.Username);
+                command.Parameters.AddWithValue("@password", personal.Usuario.Password);
+                command.Parameters.AddWithValue("@esAdmin", personal.Usuario.EsAdmin);
+                Methods.ExecuteBasicCommand(command);
+                personal.Usuario.IdUsuario = Methods.GetActIDTable("Usuario");
+
+                command = Methods.CreateBasicCommand(queryString1);
                 command.Parameters.AddWithValue("@nombre", personal.Nombre);
-                command.Parameters.AddWithValue("@PrimerApellido", personal.PrimerApellido);
+                command.Parameters.AddWithValue("@primerApellido", personal.PrimerApellido);
                 command.Parameters.AddWithValue("@segundoApellido", personal.SegundoApellido);
                 command.Parameters.AddWithValue("@sexo", personal.Sexo);
                 command.Parameters.AddWithValue("@fechaNacimiento", personal.FechaNacimiento);
                 command.Parameters.AddWithValue("@correo", personal.Correo);
+                command.Parameters.AddWithValue("@usuario", personal.Usuario.IdUsuario);
+                Methods.ExecuteBasicCommand(command);
+                personal.IdPersona = Methods.GetActIDTable("Persona");
+
+                command = Methods.CreateBasicCommand(queryString2);
+                command.Parameters.AddWithValue("@idPersona", personal.IdPersona);
                 command.Parameters.AddWithValue("@codPersonal", personal.CodPersonal);
                 command.Parameters.AddWithValue("@ci", personal.Ci);
                 command.Parameters.AddWithValue("@fechaIngreso", personal.FechaIngreso);
-                command.Parameters.AddWithValue("@cargo", personal.Cargo);
+                command.Parameters.AddWithValue("@fechaIngreso", personal.Cargo);
                 command.Parameters.AddWithValue("@sueldo", personal.Sueldo);
                 command.Parameters.AddWithValue("@turno", personal.Turno);
                 Methods.ExecuteBasicCommand(command);
+                
             }
             catch (SqlException ex)
             {
---
+++ Tintoreria/TintoreriaDAL/TelefonoDal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Upds.Sistemas.ProgWeb2.Tintoreria.Core;
+
+namespace Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaDAL
+{
+    public class TelefonoDal
+    {
+        public static void Insertar(Telefono telefono)
+        {
+            Methods.GenerateLogsDebug("TelefonoDal", "Insertar", string.Format("{0} Info: {1}",
+            DateTime.Now.ToLongDateString(), "Empezando a ejecutar el metodo acceso a datos para crear un telefono"));
+
+            SqlCommand command = null;
+
+            //Consulta para insertar telefonos
+            string queryString = @"INSERT INTO Telefono(idTelefono, Numero, tipo, idPersona) VALUES(@idTelefono, @numero, @tipo, @idPersona)";
+        
+            try
+            {
+                command = Methods.CreateBasicCommand(queryString);
+                command.Parameters.AddWithValue("@idTelefono", telefono.IdTelefono);
+                command.Parameters.AddWithValue("@numero", telefono.IdTelefono);
+                command.Parameters.AddWithValue("@tipo", telefono.Tipo);
+                command.Parameters.AddWithValue("@idPersona", telefono.idPersona);
+                Methods.ExecuteBasicCommand(command);
+
+            }
+            catch (SqlException ex)
+            {
+                Methods.GenerateLogsRelease("TelefonoDal", "Insertar", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                Methods.GenerateLogsRelease("TelefonoDal", "Insertar", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+
+            Methods.GenerateLogsDebug("TelefonoDal", "Insertar", string.Format("{0} {1} Info: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), "Termino de ejecutar  el metodo acceso a datos para insertar telefono"));
+        }
+    }
+}
