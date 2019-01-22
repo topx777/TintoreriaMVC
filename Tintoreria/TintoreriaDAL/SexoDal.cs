1141a4f065977440daa6928fbf65c2e6dd84efbf
Pablo Fernando H. Y. <pablohy2898@hotmail.com>
2019-01-22 15:26:16 -0400

    Dal Cliente, Persona, Sexo, Telefono

---
+++ Tintoreria/TintoreriaDAL/SexoDal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+using Upds.Sistemas.ProgWeb2.Tintoreria.Core;
+
+namespace Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaDAL
+{
+    class SexoDal
+    {
+        public static Sexo Get(int id)
+        {
+            Sexo res = new Sexo();
+            SqlCommand cmd = null;
+            SqlDataReader dr = null;
+            string query = @"SELECT * FROM Sexo WHERE IdSexo=@id";
+            try
+            {
+                cmd = Methods.CreateBasicCommand(query);
+                cmd.Parameters.AddWithValue("@id", id);
+                dr = Methods.ExecuteDataReaderCommand(cmd);
+                while (dr.Read())
+                {
+                    res = new Sexo()
+                    {
+                        IdSexo=dr.GetInt32(0),
+                        Nombre=dr.GetString(1)
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                Methods.GenerateLogsRelease("SexoDal", "Obtenet(Get)", string.Format("{0} {1} Error: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), ex.Message));
+                throw ex;
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+            return res;
+        }
+
+    }
+}
