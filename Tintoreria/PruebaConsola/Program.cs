0c62c2ef77cf0f2c3348c642ee5c4f9336a0d44d
Abel Lopez Paniagua <topex-555@hotmail.com>
2019-01-22 13:43:31 -0400

    Creado Proyecto Consola

---
+++ Tintoreria/PruebaConsola/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upds.Sistemas.ProgWeb2.Tintoreria.PruebaConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
