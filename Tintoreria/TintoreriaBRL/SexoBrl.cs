1384e42bc2af02dd3aeb9a6ff06809fd92924949
Pablo Fernando H. Y. <pablohy2898@hotmail.com>
2019-01-25 09:40:14 -0400

    clases Brl

---
+++ Tintoreria/TintoreriaBRL/SexoBrl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaBRL
+{
+    public class SexoBrl
+    {
+    }
+}
