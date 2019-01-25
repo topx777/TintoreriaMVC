9ea133800276b154840dcd1843dbf603c473fac6
Pablo Fernando H. Y. <pablohy2898@hotmail.com>
2019-01-25 10:54:10 -0400

    using brl

--- Tintoreria/TintoreriaBRL/PersonaBrl.cs
+++ Tintoreria/TintoreriaBRL/PersonaBrl.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaBRL
+﻿namespace Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaBRL
 {
     public class PersonaBrl
     {
