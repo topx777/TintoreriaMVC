7f304860ff2b027b5bca3e3b433baf92dcb91c88
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-01-25 11:02:45 -0400

    Cambios SexoBrl

--- Tintoreria/TintoreriaBRL/SexoBrl.cs
+++ Tintoreria/TintoreriaBRL/SexoBrl.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Upds.Sistemas.ProgWeb2.Tintoreria.Core;
+using Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaDAL;
 namespace Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaBRL
 {
     public class SexoBrl
     {
+        public static Sexo Get(int id)
+        {
+           Sexo sexo = null;
+            try
+            {
+                sexo = SexoDal.Get(id);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return sexo;
+        }
     }
 }
