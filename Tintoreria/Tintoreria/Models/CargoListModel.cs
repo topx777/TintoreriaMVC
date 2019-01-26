16a9d86583bd57770ad49bb6f8ff46bea738f997
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-01-26 11:36:28 -0400

    Cambios modulo admin cargo, controlador y modelo

---
+++ Tintoreria/Tintoreria/Models/CargoListModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaBRL;
+
+namespace Upds.Sistemas.ProgWeb2.Tintoreria.MVC.Models
+{
+    public class CargoListModel : List<CargoModel>
+    {
+        public static CargoListModel Get()
+        {
+            CargoListModel _categoriaListModel = new CargoListModel();
+            foreach (var cargo in CargoListBrl.Get())
+            {
+                _categoriaListModel.Add(new CategoriaModel
+                {
+                    IdCargo= cargo.IdCargo,
+                    Nombre = cargo.Nombre,
+                });
+            }
+            return _cargoListModel;
+        }
+    }
+}
---
+++ Tintoreria/TintoreriaDAL/CargoListDal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Upds.Sistemas.ProgWeb2.Tintoreria.Core;
+
+namespace Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaDAL
+{
+    public class CargoListDal
+    {
+        public static CargoList Get()
+        {
+            CargoList res = new CargoList();
+
+            SqlCommand cmd = null;
+            SqlDataReader dr = null;
+
+            string query = "SELECT * FROM Cargo";
+            try
+            {
+                cmd = Methods.CreateBasicCommand(query);
+                dr = Methods.ExecuteDataReaderCommand(cmd);
+
+                while (dr.Read())
+                {
+                    res.Add(new Cargo()
+                    {
+                        IdCargo = dr.GetInt32(0),
+                        Nombre = dr.GetString(1),
+                    });
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                //Methods.GenerateLogsRelease("KeyValuePacienteListDal", "Obtener", ex.Message + " " + ex.StackTrace);
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //Methods.GenerateLogsRelease("KeyValuePacienteListDal", "Obtener", ex.Message + " " + ex.StackTrace);
+                throw ex;
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+            return res;
+        }
+    }
+}
