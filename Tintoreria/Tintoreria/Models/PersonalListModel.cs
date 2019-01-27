e3bd2520a58cc11213efbbc20d85e94118dedcfa
Mauritius Gamarra <mauriciogamarravargas@gmail.com>
2019-01-27 19:18:12 -0400

    ultimos cambios del dia Mauricio Persona y Personal

--- Tintoreria/Tintoreria/Models/PersonalListModel.cs
+++ Tintoreria/Tintoreria/Models/PersonalListModel.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Upds.Sistemas.ProgWeb2.Tintoreria.TintoreriaBRL;
 
 namespace Upds.Sistemas.ProgWeb2.Tintoreria.MVC.Models
 {
     public class PersonalListModel:List<PersonalModel>
     {
-
+        /// <summary>
+        /// Necesita ser public, cuidado todo en poner public
+        /// </summary>
+        /// <returns></returns>
+        public static PersonalListModel Get()
+        {
+            PersonalListModel _personalListModel = new PersonalListModel();
+            foreach (var personal in PersonalListBrl.Get())
+            {
+                _personalListModel.Add(new PersonalModel
+                {
+                    
+                });
+            }
+            return _personalListModel;
+        }
     }
 }
